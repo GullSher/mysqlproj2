805be39ba4a0cd50b36bbe24056c48185c962348
Gull-Sher <mobilegull73@gmail.com>
2023-12-11 22:48:03 +0500

    aa

---
+++ src/main/java/net/javaguides/hibernate/util/HibernateUtil.java
@@ -0,0 +1,26 @@
+package net.javaguides.hibernate.util;
+
+public class HibernateUtil {
+
+//	<session-factory> 
+
+//	  Copy Blow lines under session-factory tags. 	
+	// <property name="connection.driver_class">com.mysql.jdbc.Driver</property>
+//	<property name="connection.url">jdbc:mysql://localhost:8080/crud</property> 
+//	 		<property name="connection.url">jdbc:mysql://localhost:8080/crud</property> 
+//	<property name="connection.url">jdbc:mysql://localhost:3306/student_mangae</property> 
+//		<property name="connection.username">root</property> 
+//		<property name="connection.password">12345</property> 
+//	 <property name="connection.username">admin</property> 
+//	<property name="connection.password">tomcat</property> 
+//	 		<property name="connection.username">root</property> 
+//	 		<property name="connection.password"></property> 
+//	 		<property name="dialect">org.hibernate.dialect.MySQL5Dialect</property> 
+//	 		<property name="hbm2ddl.auto">update</property> 
+//	 		<property name="show_sql">true</property> 
+//	 		<property name="format_sql">true</property> 
+//	 		<mapping class="com.entities.Note"/> 
+	// Copy Upper lines under session-factory tags.
+//		</session-factory> -->
+
+}
